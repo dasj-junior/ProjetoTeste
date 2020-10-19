ec1755aab61b77d2830e933c83168f14c27ad7e6
Dorival Junior <dasj.junior@gmail.com>
2020-10-19 08:17:14 -0300

    Projeto conta bancaria funcionando corretamente

---
+++ PrimeiroProjeto/Conta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace PrimeiroProjeto
+{
+    class Conta
+    {
+        private int _numero;
+        private string _titular;
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            _numero = numero;
+            _titular = titular;
+        }
+
+        public Conta(int numero, string titular, double valordeposito) : this (numero, titular)
+        {
+            if (valordeposito > 0.00)
+            {
+                Saldo += valordeposito;
+            }
+        }
+
+        public void Deposito(double valordeposito)
+        {
+            if (valordeposito > 0.00)
+            {
+                Saldo += valordeposito;
+            }
+        }
+
+        public void Saque(double valorsaque)
+        {
+            if (valorsaque > 0.00)
+            {
+                Saldo -= (valorsaque + 5.00);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta " + _numero + ", Titular: " + _titular + ", Saldo: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroconta;
+            string titularconta;
+            char depositoinicial;
+            double valordeposito = 0.00;
+            double valorsaque = 0.00;
+
+
+            Console.Write("Entre o número da conta: ");
+            numeroconta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            titularconta = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            depositoinicial = char.Parse(Console.ReadLine());
+
+            Conta conta;
+            if (depositoinicial == 's')
+            {
+                Console.Write("Entre um valor para o depósito inicial: ");
+                valordeposito = double.Parse(Console.ReadLine());
+                Console.Write("\n");
+
+                conta = new Conta(numeroconta,titularconta,valordeposito);
+            }
+            else
+            {
+                Console.Write("\n");
+                conta = new Conta(numeroconta, titularconta);
+            }
+            
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta.ToString() + "\n");
+
+            Console.Write("Entre um valor para depósito: ");
+            valordeposito = double.Parse(Console.ReadLine());
+
+            conta.Deposito(valordeposito);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta.ToString() + "\n");
+
+            Console.Write("Entre um valor para saque: ");
+            valorsaque = double.Parse(Console.ReadLine());
+
+            conta.Saque(valorsaque);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta.ToString() + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
